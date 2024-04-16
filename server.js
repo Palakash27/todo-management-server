c589269394d37823ca44b1f935e33b57ac26c562
Aakash Pal <palakash27@gmail.com>
2024-04-16 12:57:01 -0400

    Add API root route

--- server.js
+++ server.js
@@ -38,6 +38,12 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
+
+// API HOME
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
 app.listen(
     PORT,
     console.log(`Server running on http://localhost:${PORT}`.warn)
